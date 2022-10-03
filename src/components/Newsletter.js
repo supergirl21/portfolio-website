407d51be1fb0cdf5cfb90cde3b62f356c21af8a0
supergirl <supergirl@anniecannons.com>
2022-10-03 15:03:10 -0700

    update Newletter handlesubmit

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -8,14 +8,14 @@ function Newsletter(onValidated, status, message) {
     if (status === "success") clearFields();
   }, [status]);
 
-  //   const handleSubmit = (e) => {
-  //     e.preventDefault();
-  //     email &&
-  //     email.indexOf(@)> -1 &&
-  //     onValidated({
-  //         EMAIL: email
-  //     })
-  //   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //   email &&
+    //   email.indexOf(@)> -1 &&
+    //   onValidated({
+    //       EMAIL: email
+    //   })
+  };
 
   const clearFields = () => {
     setEmail("");
