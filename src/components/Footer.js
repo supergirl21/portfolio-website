eda8b61bb246ad34eafe3cba16e12589027f5cdf
supergirl <supergirl@anniecannons.com>
2022-10-03 15:00:50 -0700

    added mailchilp subscriber, newsletter and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,48 @@
+import MailchimpForm from "./MailchimpForm";
+import { Container, Row, Col } from "react-bootstrap";
+import logo from "../assets/img/logo4.png";
+import navIcon1 from "../assets/img/nav-icon1.svg";
+import navIcon2 from "../assets/img/nav-icon2.svg";
+import navIcon3 from "../assets/img/nav-icon3.svg";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row className="align-item-center">
+          <MailchimpForm />
+          <Col sm={5}>
+            <img src={logo} alt="Logo" />
+          </Col>
+          <Col sm={6} className="text-center text-sm-end">
+            <div className="social-icon">
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.linkedin.com/in/dona-g"
+              >
+                <img src={navIcon1} alt="linked-in" />
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.facebook.com/"
+              >
+                <img src={navIcon2} alt="facebook" />
+              </a>
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href="https://www.instagram.com/dona_gino/"
+              >
+                <img src={navIcon3} alt="instagram" />
+              </a>
+            </div>
+            <p>CopyRight 2022. All Rights Reserved Dona Gino</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+}
+export default Footer;
---
+++ src/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+
+// server ued to send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+app.listen(5000, () => console.log("Server Running"));
+console.log(process.env.EMAIL_USER);
+console.log(process.env.EMAIL_PASS);
+
+const contactEmail = nodemailer.createTransport({
+  services: "gmail",
+  auth: {
+    user: "********@gmail.com",
+    pass: "",
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: "********@gmail.com",
+    subject: "Contact Form Submission - Postfolio",
+    html: `<p>Name: ${name}</p>
+                <p>Name: ${email}</p>
+                <p>Name: ${phone}</p>
+                <p>Name: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      req.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
